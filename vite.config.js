36cba6b9c436faa70d59c4540c4dd81cb9f08f5d
opiano <opiano@naver.com>
2026-05-25 23:43:27 +0900

    Configure conditional base path for automatic Vercel and local Pi builds

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/fbd-editor/',
+  base: process.env.VERCEL ? '/' : '/fbd-editor/',
   plugins: [vue()],
 })
 
